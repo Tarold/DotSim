61b1efc5e60acdcb6a0f8724ee942afbcfb3d044
‘Tarold’ <ltdecorer@gmail.com>
2022-11-01 16:49:22 +0200

    Added dot food

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ window.addEventListener("load", function () {
   table.style.width = "100px";
   globalThis.dotList = [];
   globalThis.dotCount = 0;
+  globalThis.foodList = [];
+  globalThis.foodCount = 0;
   table.addEventListener("click", addDot);
 });
 
@@ -49,20 +51,39 @@ function moveDot(element, x, y) {
 }
 
 function addDot(event) {
-  const card = document.createElement("div");
-  card.className = "dot";
-  card.id = dotCount;
-  card.setAttribute(
-    "style",
-    `left:${event.offsetX}px; top:${event.offsetY}px;`
-  );
-  card.dataset.hungry = 4;
-  table.insertAdjacentElement("afterbegin", card);
+  const otherCheckbox = document.querySelector("#horns");
+  console.log("otherCheckbox", otherCheckbox);
+  if (otherCheckbox.checked) {
+    const food = document.createElement("div");
+    food.className = "food";
+    food.id = foodCount + "f";
+    food.setAttribute(
+      "style",
+      `left:${event.offsetX}px; top:${event.offsetY}px;`
+    );
+    food.dataset.satiety = 2;
+    table.insertAdjacentElement("afterbegin", food);
 
-  dotList.push({
-    name: "dot",
-    id: dotCount++,
-  });
+    foodList.push({
+      name: "dot",
+      id: foodCount++ + "f",
+    });
+  } else {
+    const card = document.createElement("div");
+    card.className = "dot";
+    card.id = dotCount;
+    card.setAttribute(
+      "style",
+      `left:${event.offsetX}px; top:${event.offsetY}px;`
+    );
+    card.dataset.hungry = 4;
+    table.insertAdjacentElement("afterbegin", card);
+
+    dotList.push({
+      name: "dot",
+      id: dotCount++,
+    });
+  }
 }
 
 function pxToInt(variable) {
