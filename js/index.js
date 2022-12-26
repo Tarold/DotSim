352422750ab534aa70758be23516579de4d2968e
‘Tarold’ <ltdecorer@gmail.com>
2022-12-26 05:28:50 +0200

    Added code refactoring
    Added constructor for SimSystem

--- js/index.js
+++ js/index.js
@@ -5,61 +5,11 @@ let table = document.querySelector('.table');
 table.style.height = '50px';
 table.style.width = '50px';
 
-table.addEventListener('click', addDot);
-
 const sys = new SimSystem();
 
 setInterval(function () {
-  init();
+  sys.init();
 }, 1000);
-
-function init() {
-  if (sys.dotList.length != 0) {
-    sys.dotList.forEach((element, i) => {
-      setTimeout(() => {
-        element.think();
-      }, i * (1000 / sys.dotList.length));
-    });
-  }
-}
-
-function addDot(event) {
-  const otherCheckbox = document.querySelector('#horns');
-  if (otherCheckbox.checked) {
-    const food = document.createElement('div');
-    food.className = 'food';
-    food.id = sys.foodCount + 'f';
-    food.setAttribute(
-      'style',
-      `left:${event.offsetX}px; top:${event.offsetY}px; z-index:0;`
-    );
-    food.dataset.satiety = 0.2;
-    table.insertAdjacentElement('afterbegin', food);
-
-    sys.foodList.push(
-      new Food(
-        'dot',
-        document.getElementById(sys.foodCount + 'f'),
-        sys.foodCount++ + 'f',
-        false
-      )
-    );
-  } else {
-    const card = document.createElement('div');
-    card.className = 'dot';
-    card.id = sys.dotCount;
-    card.setAttribute(
-      'style',
-      `left:${event.offsetX}px; top:${event.offsetY}px; z-index:1;`
-    );
-    card.dataset.hungry = 4;
-    table.insertAdjacentElement('afterbegin', card);
-
-    sys.dotList.push(
-      new Dot('dot', document.getElementById(sys.dotCount), sys.incDotCount())
-    );
-  }
-}
 
 function SimSystem() {
   this.dotList = [];
@@ -68,8 +18,71 @@ function SimSystem() {
   this.foodCount = 0;
 }
 
+SimSystem.prototype.init = function () {
+  if (this.dotList.length != 0) {
+    this.dotList.forEach((dot, i) => {
+      setTimeout(() => {
+        dot.think();
+      }, i * (1000 / this.dotList.length));
+    });
+  }
+};
+
+SimSystem.prototype.addFood = function (x, y) {
+  const food = document.createElement('div');
+  food.className = 'food';
+  food.id = sys.foodCount + 'f';
+  food.setAttribute('style', `left:${x}px; top:${y}px; z-index:0;`);
+  food.dataset.satiety = 0.2;
+  table.insertAdjacentElement('afterbegin', food);
+
+  sys.foodList.push(
+    new Food(
+      'dot',
+      document.getElementById(sys.foodCount + 'f'),
+      sys.incFoodCount() + 'f',
+      false
+    )
+  );
+};
+
+SimSystem.prototype.addDot = function (x, y) {
+  const dot = document.createElement('div');
+  dot.className = 'dot';
+  dot.id = sys.dotCount;
+  dot.setAttribute('style', `left:${x}px; top:${y}px; z-index:1;`);
+  dot.dataset.hungry = 4;
+  table.insertAdjacentElement('afterbegin', dot);
+
+  sys.dotList.push(
+    new Dot('dot', document.getElementById(sys.dotCount), sys.incDotCount())
+  );
+};
+
+SimSystem.prototype.removeObject = function (obj) {
+  let list = obj.id.includes('f') ? this.foodList : this.dotList;
+
+  list = list.filter(function (value) {
+    return value.id != obj.id;
+  });
+  obj.remove();
+};
+
 SimSystem.prototype.incDotCount = function () {
   return this.dotCount++;
+};
+
+SimSystem.prototype.incFoodCount = function () {
+  return this.foodCount++;
+};
+
+SimSystem.prototype.eatDot = function (dot, food) {
+  dot.setHungry = Number(food.dataset.satiety); //TODO make get satiety
+  this.removeObject(food);
+  sys.addFood(
+    (Math.random() * pxToInt(table.style.width)) | 0,
+    (Math.random() * pxToInt(table.style.height)) | 0
+  );
 };
 
 function Food(name, object, id) {
@@ -86,17 +99,18 @@ function Dot(name, object, id) {
   this.movingTo = undefined;
 }
 
-//коли точка їсть, переноситься до кінця списку
+//TODO коли точка поїсть, вона переноситься до кінця списку, щоб поїли ті, що не їли
+//TODO точки повинні з'являтися на наступному ході(додати стан появи, або созрівання)
+//TODO коли немає їжі точка швидко зникає
 
 Dot.prototype.think = function () {
   if (this.movingTo) {
-    eatDot(this.object, this.movingTo);
+    sys.eatDot(this.object, this.movingTo);
     this.movingTo = false;
   }
   const dotStyle = this.object.style;
   let minDist = [Infinity];
   sys.foodList.forEach((food) => {
-    console.log('food :>> ', food);
     if (!food.reserved) {
       const foodStyle = food.object.style;
       const dist = distance(
@@ -167,8 +181,12 @@ Dot.prototype.moveDot = function (x, y) {
     element.dataset.hungry -= 0.1;
   } else {
     element.dataset.hungry = 0;
-    deadDot(element);
+    sys.removeObject(element);
   }
+};
+
+Dot.prototype.setHungry = function (number) {
+  this.object.dataset.hungry += number;
 };
 
 function pxToInt(variable) {
@@ -176,51 +194,17 @@ function pxToInt(variable) {
   return num;
 }
 
-function eatDot(dot, food) {
-  dot.dataset.hungry =
-    Number(dot.dataset.hungry) + Number(food.dataset.satiety);
-  deadFood(food);
-  createFood();
-}
-
-function deadDot(element) {
-  dotList = dotList.filter(function (value) {
-    return value.id != element.id;
-  });
-  element.remove();
-}
-
-function deadFood(element) {
-  sys.foodList = sys.foodList.filter(function (value) {
-    return value.id != element.id;
-  });
-  element.remove();
-}
-
-function createFood() {
-  const food = document.createElement('div');
-  food.className = 'food';
-  food.id = sys.foodCount + 'f';
-  food.setAttribute(
-    'style',
-    `left:${(Math.random() * pxToInt(table.style.width)) | 0}px; top:${
-      (Math.random() * pxToInt(table.style.height)) | 0
-    }px; z-index:0;`
-  );
-  food.dataset.satiety = 0.2;
-  food.dataset.reserved = false;
-  table.insertAdjacentElement('afterbegin', food);
-
-  sys.foodList.push(
-    new Food(
-      'dot',
-      document.getElementById(sys.foodCount + 'f'),
-      sys.foodCount++ + 'f',
-      false
-    )
-  );
-}
-
 function distance(x1, y1, x2, y2) {
   return Math.hypot(x2 - x1, y2 - y1);
+}
+
+table.addEventListener('click', addDot);
+
+function addDot(event) {
+  const otherCheckbox = document.querySelector('#horns');
+  if (otherCheckbox.checked) {
+    sys.addFood(event.offsetX, event.offsetY);
+  } else {
+    sys.addDot(event.offsetX, event.offsetY);
+  }
 }
